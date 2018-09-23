32da7f03adea0aa00ee1ea3fe815095bf5a13785
damith <damithasanka@gmail.com>
2018-09-23 14:15:45 +0530

    add testing comments

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import codePush from "react-native-code-push";
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,6 +19,9 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  componentDidMount(){
+    codePush.sync({ mandatoryInstallMode: codePush.InstallMode.ON_NEXT_RESUME });
+  }
   render() {
     return (
       <View style={styles.container}>
